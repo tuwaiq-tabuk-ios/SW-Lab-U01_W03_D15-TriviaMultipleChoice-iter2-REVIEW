ee0fa35808af908331980c9ddbc2d85c95322f75
ahemdalq <91871668+ahemdalq@users.noreply.github.com>
2021-11-25 16:29:40 +0300

    finished

---
+++ TriviaMultipleChoice-Ahmed-Alqhtani-iter3/TriviaMultipleChoice-Ahmed-Alqhtani-iter3/models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  TriviaMultipleChoice-Ahmed-Alqhtani-iter3
+//
+//  Created by Ahmed awadh alqhtani on 11/04/1443 AH.
+//
+
+import UIKit
+
+struct Question {
+  let question:String
+  let answers: [String]
+  let correctAnswer:String
+}
+
