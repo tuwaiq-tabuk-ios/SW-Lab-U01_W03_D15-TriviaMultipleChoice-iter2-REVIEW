363926cf6540596be745ba9e5823d1c9d6d8e9fc
mohammed <mhmdalsbwty@gmail.com>
2021-11-25 16:20:06 +0300

    Project2

---
+++ TriviaMultipleChoice_mohammed_alatawi_ Project2 2 2 2/TriviaMultipleChoice_mohammed_alatawi_ Project2/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  TriviaMultipleChoice_mohammed_alatawi_ Project2
+//
+//  Created by محمد العطوي on 08/04/1443 AH.
+//
+
+import Foundation
+
+import Foundation
+
+struct Question {
+  let question: String
+  let answers: [String]
+  let correctAnswer: String
+  
+}
+
