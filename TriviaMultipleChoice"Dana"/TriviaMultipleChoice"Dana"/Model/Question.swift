411c41172c872e9b286fad71d0dc98f1ddcb03d4
macbook air <macbookair@Danah.local>
2021-11-25 16:12:53 +0300

    finish

---
+++ TriviaMultipleChoice"Dana"/TriviaMultipleChoice"Dana"/Model/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  TriviaMultipleChoice"Dana"
+//
+//  Created by macbook air on 15/3/1443 AH.
+
+//
+
+import Foundation
+struct Question { //convention to make struct the file name
+  let question: String
+  let choices: [String]
+  let answer: String
+    
+  }
+
